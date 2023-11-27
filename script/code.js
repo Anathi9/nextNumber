6d068981fde57beb4957d8d2438144d6ef4873f3
Anathi9 <anathijosefu@gmail.com>
2023-11-27 15:16:27 +0200

    changes

--- script/code.js
+++ script/code.js
@@ -1,7 +1,79 @@
-function nextNumber(...arg){
-    let count = 0;
-    while(count < arg.length)
-    console.log(arg[count])
-count++;
-}
-console.log(nextNumber(2, 4, 5, 7, 8, 11))
+
+//ageTODays
+// function ageToDays(age) {
+//     let currerntYear = new Date()
+//         let myAge = currerntYear.getFullYear() - age;
+//         let days = (currerntYear.getFullYear() - my Age) * 365
+//         console.log(daysLived);
+// }
+// ageToDays(24)
+
+//areaOfTriangle
+// function areaOFtriangle(base,height) {
+//     let calculateArea = (base * height) /2
+//     console.log(calculateArea);
+
+// }
+// areaOFtriangle(10, 2)
+// add up
+// function array(...arg) {
+//     console.log(arg)
+//     let sum = 0
+//     for(let i = 0; i<arg.length;i++){
+//         console.log(sum+=arg[i]);
+//     }
+// }
+// array(2, 5, 6, 9 )
+
+
+
+// // interes
+// function interest(amount,interest) {
+//     let calcuInterest =(amount * interest /100)
+//     console.log(interest+"% of "+amount+" is "+interest)
+// }
+// interest(300,30)
+   
+    
+// function nextNumber(...arg){
+//     let count = 0;
+//     while(count < arg.length){
+
+//         console.log(arg[count])
+//     count++;
+//     }
+// }
+// console.log(nextNumber(2, 5, 6, 7, 9, 12))
+
+// minMax
+// function minMax(...array) {
+// let max = Math.max(array)
+// console.log(`Highest number: ${Math.max(...array)}`)
+
+// let min = Math.min(array)
+// console.log(`Lowest number: ${Math.min(...array)}`)
+// }
+// minMax(20, 15, 9, 7, 5, 2)
+
+//count truthy
+    // let boolean = [true,  false, true, false];
+    // let truecount = boolean.reduce((count, value) => value ? count + 1 : count, 0);
+    // console.log(`Number of true value: ${truecount}`)
+
+
+    hexToRGB
+    function hexToRGB(color) {
+        hex = hex.replace('')
+        let R = parseInt(0, 2)
+        let G = ParseInt(2,4 )
+        let B = parseInt(4,6)
+        console.lo(RGB)
+    }
+
+
+
+
+
+
+
+
